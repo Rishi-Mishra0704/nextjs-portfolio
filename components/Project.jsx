e276ef5e5dfd61ee31f052e33b8af613049ed2fc
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-10 01:48:08 +0530

    Update projects

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Image from "next/legacy/image";
-import budget from "../public/assets/budget-buddy.png";
+import resto from "../public/assets/resto.png";
 import carhub from "../public/assets/new-carhub.png";
 import expense from "../public/assets/expense-racker.png";
 import conference from "../public/assets/conference.png";
+import nextboard from "../public/assets/nextboard.png";
 
 const Project = () => {
   const nextButton = <button className="next-btn ml-1 w-40 h-10 ">Next</button>;
@@ -49,10 +50,16 @@ const Project = () => {
             <Image src={conference} />
           </div>
           <div className="mb-10">
-            <a href="https://rishi-mishra0704.github.io/Capstone/index.html" target="_blank">
+            <a
+              href="https://rishi-mishra0704.github.io/Capstone/index.html"
+              target="_blank"
+            >
               <button className="h-10 w-40 mx-1 my-8">Live demo</button>
             </a>
-            <a href="https://github.com/Rishi-Mishra0704/Capstone" target="_blank">
+            <a
+              href="https://github.com/Rishi-Mishra0704/Capstone"
+              target="_blank"
+            >
               <button className="h-10 w-40 mx-1 my-8">Source Code</button>
             </a>
           </div>
@@ -83,7 +90,42 @@ const Project = () => {
             <a href="https://expense-racker.onrender.com/" target="_blank">
               <button className="h-10 w-40 mx-1 my-8">Live demo</button>
             </a>
-            <a href="https://github.com/Rishi-Mishra0704/react-project" target="_blank">
+            <a
+              href="https://github.com/Rishi-Mishra0704/react-project"
+              target="_blank"
+            >
+              <button className="h-10 w-40 mx-1 my-8">Source Code</button>
+            </a>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2">
+          <div className="">
+            <p className="text-xl my-2  tracking-widest text-center uppercase text-[#1f2937] dark:text-[#ecf0f3]">
+              NextBoard
+            </p>
+            <p className="text-l tracking-widest text-center uppercase text-[#1f2937] dark:text-[#ecf0f3]">
+              Built with Nextjs and tailwindcss. It is a dashboard site which's
+              primary purpose was to test out my styling skills along with
+              typescript. 
+            </p>
+            <ul className="mt-20">
+              <li>1. Next.js</li>
+              <li>2. Tailwind CSS</li>
+              <li>3. Typescript</li>
+              <li>3. GIT/GITHUB</li>
+            </ul>
+          </div>
+          <div className="border-solid border-2 dark:border-teal-400 px-1 py-0.5 m-3">
+            <Image src={nextboard} />
+          </div>
+          <div>
+            <a href="https://rishi-nextboard.vercel.app/" target="_blank">
+              <button className="h-10 w-40 mx-1 my-8">Live demo</button>
+            </a>
+            <a
+              href="https://github.com/Rishi-Mishra0704/next_dashboard"
+              target="_blank"
+            >
               <button className="h-10 w-40 mx-1 my-8">Source Code</button>
             </a>
           </div>
@@ -117,8 +159,17 @@ const Project = () => {
             <a href="https://carhub-2.vercel.app/" target="_blank">
               <button className="h-10 w-40 mx-1 my-8">Live demo</button>
             </a>
-            <a href="https://github.com/Rishi-Mishra0704/carhub-frontend" target="_blank">
-              <button className="h-10 w-40 mx-1 my-8">Source Code</button>
+            <a
+              href="https://github.com/Rishi-Mishra0704/carhub-backend"
+              target="_blank"
+            >
+              <button className="h-10 w-40 mx-1 my-8">Front-end Code</button>
+            </a>
+            <a
+              href="https://github.com/Rishi-Mishra0704/carhub-frontend"
+              target="_blank"
+            >
+              <button className="h-10 w-40 mx-1 my-8">Front-end Code</button>
             </a>
           </div>
         </div>
@@ -129,30 +180,36 @@ const Project = () => {
               Your Budget Buddy
             </p>
             <p className="text-l tracking-widest text-center uppercase text-[#1f2937] dark:text-[#ecf0f3]">
-              This is a budget tracker that helps you keep track of all your
-              expenses and income. This was primarily built on ruby on rails and
-              bootstrap . It has a special feature namely devise to help you
-              login and logout of your account. The database used was postgres
-              to store all the data. It was built to test my skills in ruby on
-              rails and bootstrap.
+              Developed a dynamic food ordering WebApp with API integration,
+              facilitating seamless meal retrieval and order posting. Improved
+              load time by 30% through optimized CSS modules.
             </p>
             <ul className="mt-20">
-              <li>1. Ruby on rails</li>
-              <li>2. Bootsrap</li>
-              <li>3. Postgres</li>
-              <li>4. GIT/GITHUB</li>
-              <li>5. Devise</li>
+              <li>1. React.js</li>
+              <li>2. Css Modules</li>
+              <li>3. MongoDB</li>
+              <li>4. Node/Express.js</li>
+              <li>5. GIT/GITHUB</li>
             </ul>
           </div>
           <div className="border-solid border-2 dark:border-teal-400 px-1 py-0.5 m-3">
-            <Image src={budget} />
+            <Image src={resto} />
           </div>
           <div>
-            <a href="https://budgetbuddy-vtim.onrender.com" target="_blank">
+            <a href="https://rishi-resto.vercel.app/" target="_blank">
               <button className="h-10 w-40 mx-1 my-8">Live demo</button>
             </a>
-            <a href="https://github.com/Rishi-Mishra0704/YourBudgetBuddy" target="_blank">
-              <button className="h-10 w-40 mx-1 my-8">Source Code</button>
+            <a
+              href="https://github.com/Rishi-Mishra0704/rishi-resto"
+              target="_blank"
+            >
+              <button className="h-10 w-40 mx-1 my-8">Front-end Code</button>
+            </a>
+            <a
+              href="https://github.com/Rishi-Mishra0704/resto-backend"
+              target="_blank"
+            >
+              <button className="h-10 w-40 mx-1 my-8">Back-end Code</button>
             </a>
           </div>
         </div>
